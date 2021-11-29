38dbe20540e1a68b1629ce09b3b45d3587eec01e
til_lsanchez <til_lsanchez@uolinc.com>
2021-11-29 15:42:16 -0300

    sync

--- src/main/java/com/github/lionani07/appvideos/model/Video.java
+++ src/main/java/com/github/lionani07/appvideos/model/Video.java
@@ -1,5 +1,6 @@
 package com.github.lionani07.appvideos.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ public class Video {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty("user_id")
     private Long userId;
     private String name;
     private String url;
